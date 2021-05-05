e08afb9f55b89d6ab5543ecd9e832d152ac30276
Samvit Chrungoo <samchrungoo.sc@gmail.com>
2021-05-05 12:11:09 +0530

    :rocket: Custom icons added

---
+++ lib/constants/icons/notes_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class NotesIcons {
+  NotesIcons._();
+
+  static const _kFontFam = 'NotesIcons';
+  static const _kFontPkg = null;
+
+  static const IconData noteEdit =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData noteSearch =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
